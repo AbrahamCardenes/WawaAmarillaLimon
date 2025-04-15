d2d9fa985d3fab117bed018474f850e8df214a7a
AbrahamCardenes <abrahamcardenesmoreno@gmail.com>
2025-04-15 18:35:35 +0100

    update: extracts CrashlyticsServiceImpl file

--- core_android/src/main/java/com/abrahamcardenes/core_android/firebase/CrashlyticsService.kt
+++ core_android/src/main/java/com/abrahamcardenes/core_android/firebase/CrashlyticsService.kt
@@ -1,18 +1,6 @@
 package com.abrahamcardenes.core_android.firebase
 
-import com.google.firebase.crashlytics.FirebaseCrashlytics
-
 interface CrashlyticsService {
     suspend fun logException(throwable: Throwable)
     suspend fun logException(e: Exception)
-}
-
-class CrashlyticsServiceImpl(val firebaseCrashlytics: FirebaseCrashlytics) : CrashlyticsService {
-    override suspend fun logException(throwable: Throwable) {
-        firebaseCrashlytics.recordException(throwable)
-    }
-
-    override suspend fun logException(e: Exception) {
-        firebaseCrashlytics.recordException(e)
-    }
 }

8556298641292898073583962e62971b73d92911
AbrahamCardenes <abrahamcardenesmoreno@gmail.com>
2025-08-20 10:26:24 +0100

    update:version bump.

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -2,6 +2,6 @@ object AndroidConfig {
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
     const val TARGET_SDK = 35
-    const val VERSION_CODE = 7
-    const val VERSION_NAME = "0.1.1"
+    const val VERSION_CODE = 8
+    const val VERSION_NAME = "0.1.2"
 }

8ecd77dcea7f6aab1570c9b6713230247b54bd4b
AbrahamCardenes <abrahamcardenesmoreno@gmail.com>
2026-03-07 08:30:36 +0000

    build: add androidx-compose-material-icons dependency

--- lpa_presentation/build.gradle.kts
+++ lpa_presentation/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material.icons)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.foundation)
     implementation(libs.androidx.ui.graphics)

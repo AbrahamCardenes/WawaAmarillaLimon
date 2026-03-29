c05227efa9bdfb26f41bba812793eae1d146b10d
AbrahamCardenes <abrahamcardenesmoreno@gmail.com>
2026-03-29 17:25:16 +0100

    chore: minor changes.

--- lpa_presentation/src/main/java/com/abrahamcardenes/lpa_presentation/home/BusStopsScreen.kt
+++ lpa_presentation/src/main/java/com/abrahamcardenes/lpa_presentation/home/BusStopsScreen.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -80,7 +80,7 @@ private fun BusStosScreenWithTabs(
 
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = {
                     BusStopsTabs(
                         currentPage = pagerState.currentPage,
